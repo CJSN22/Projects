9b071b7a89c62d5b082b8f2a26fd8670ce1bb85d
CJSN22 <cjsn22@gmail.com>
2022-03-16 22:32:03 -0400

    p22 drawing app push

--- Project_22/script.js
+++ Project_22/script.js
@@ -1,11 +1,18 @@
 const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
 const ctx = canvas.getContext('2d')
 
-let size = 20
+let size = 10
 let isPressed = false
 let color = 'black'
 let x 
 let y
+
 
 canvas.addEventListener('mousedown', (e) => {
     isPressed = true
@@ -37,7 +44,7 @@ canvas.addEventListener('mousemove', (e) => {
 
 
 function drawCircle(x, y) {
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2)
     ctx.fillStyle = color
     ctx.fill()
@@ -47,7 +54,36 @@ function drawLine(x1, y1, x2, y2) {
     ctx.beginPath()
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
-    ctx.stokeStyle = color
+    ctx.strokeStyle = color
     ctx.lineWidth = size * 2
     ctx.stroke()
 }
+
+function updateSizeOnScreen() {
+    sizeEl.innerText = size
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5
+
+    if(size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+
+    if(size < 5) {
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+
+clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.scrollHeight))
